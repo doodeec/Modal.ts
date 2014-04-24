541c8e67b23883c2e5e2283115b55c8de5815223
doodeec <doodeec@gmail.com>
2014-04-24 14:44:00 +0200

    opening modals

--- _all.js
+++ _all.js
@@ -2,13 +2,21 @@ define(["require", "exports", './Modal'], function(require, exports, Modal) {
     function openModal() {
         var mod = new Modal.ModalWindow();
         mod.setMessage("This is new modal window");
-        mod.addButton("OK", function () {
-            console.log("OK clicked");
-        });
+        mod.addButton("OK");
         mod.open();
-        //    mod.close();
     }
 
-    openModal();
+    function openAnotherModal() {
+        var mod = new Modal.ModalWindow();
+        mod.setMessage("This is another modal window with two buttons");
+        mod.addButton("Cancel");
+        mod.addButton("OK", function () {
+            alert("Modal will be closed now");
+        });
+        mod.open();
+    }
+
+    document.getElementById("open-modal").addEventListener("click", openModal);
+    document.getElementById("open-another").addEventListener("click", openAnotherModal);
 });
 //# sourceMappingURL=_all.js.map
